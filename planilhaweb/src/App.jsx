320567a4c3ab0b287d8340655f13c257c8d42010
Kleyton Holanda <69331002+Kehs4@users.noreply.github.com>
2025-03-13 23:20:29 -0300

    1.1

--- planilhaweb/src/App.jsx
+++ planilhaweb/src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Clientes from '../clientes'
 import getClientes from '../getclients'
+import DataTable from 'datatables.net-dt';
+ 
+let table = new DataTable('#clientes-table');
 
 
 function App() {
@@ -19,32 +22,103 @@ function App() {
           </nav>
         </header>
 
-        <div class="text-1">
-            <h3>Pesquisa dos cadastros dos clientes da GPI</h3>
-            <p>Digite o nome ou os dados da sua pesquisa abaixo.</p>
-        </div>
+      <div class="div-form-clients">
+          <form class="form-clients" action="">
+              <div class="search-clients">
+                <fieldset>
+                <legend>Dados do Cliente</legend>
+                  <label for="client-name">Nome do Cliente</label>
+                  <input type="text" id="client-name" placeholder="Nome do Cliente"></input>
+                    
+                  <label for="client-cod">Código do Cliente</label>
+                  <input type="number" id="client-cod" placeholder="Código do Cliente"></input>
 
-        <form class="form-clients" action="">
-            <div class="search-clients">
-                <label for="name-client">Nome</label>
-                <input type="text" id="name-client" placeholder="Digite o nome do cliente" required></input>
-                <label for="cod">Código do Cliente</label>
-                <input type="number" id="cod" placeholder="Digite o código do cliente"></input>
-                <button class="btn-search" type="submit">Procurar</button>
-            </div>
-        </form>
+                  <label for="client-version">Versão do Vertis</label>
+                  <input type="text" id="client-version" placeholder="Versão do Vertis"></input>
+                
+                  <label for="client-maintenance">Manutenção Realizada</label>
+                  <input type="text" id="client-maintenance" placeholder="Manutenção do Vertis"></input>
 
-        <h1 class="client-list">Lista de Clientes</h1>
+                  <label for="client-maintenance">Manutenção Futura</label>
+                  <input type="text" id="client-maintenance-future" placeholder="Próxima Manutenção"></input>
+                
+                  <label for="client-licences">Licenças da Unidade</label>
+                  <input type="text" id="client-licences" placeholder="Total de Licenças"></input>
+                </fieldset>
+                    
+                <fieldset>
+                  <legend>Dados de Acesso</legend>
+                  <label for="client-teamviewer">TeamViewer</label>
+                  <input type="text" id="client-teamviewer" placeholder="TeamViewer do cliente"></input>
+
+                  <label for="client-teamviewer-password">Senha TeamViewer</label>
+                  <input type="text" id="client-teamviewer-password" placeholder="Senha do TeamViewer"></input>
+
+                  <label for="client-anydesk">AnyDesk</label>
+                  <input type="text" id="client-anydesk" placeholder="AnyDesk do cliente"></input>
+
+                  <label for="client-anydesk-password">Senha AnyDesk</label>
+                  <input type="text" id="client-anydesk-password" placeholder="Senha do AnyDesk"></input>
+                </fieldset>
+
+                <fieldset>
+                  <legend>Dados do Endereço</legend>
+                  <label for="client-address-street">Endereço Logradouro</label>
+                  <input type="text" id="client-address" placeholder="Endereço do cliente"></input>
+
+                  <label for="client-address-number">Número Logradouro</label>
+                  <input type="number" id="client-address-number" placeholder="Número do cliente"></input>
+
+                  <label for="client-address-state">Estado</label>
+                  <input type="text" id="client-address-state" placeholder="Estado do cliente"></input>
+                
+                  <label for="client-address-city">Cidade</label>
+                  <input type="text" id="client-address-city" placeholder="Cidade do cliente"></input>
+
+                  <label for="client-address-district">Bairro</label>
+                  <input type="text" id="client-address-district" placeholder="Bairro do cliente"></input>
+
+                  <label for="client-address-cep">CEP</label>
+                  <input type="number" id="client-address-cep" placeholder="CEP do cliente"></input>
+                </fieldset>  
+                  
+                <fieldset>
+                  <legend>Contatos</legend>
+                  <label for="client-manager">Responsável Unidade</label>
+                  <input type="number" id="client-manager" placeholder='Responsável pela unid.'></input>
+                  
+                  <label for="client-email">E-mail</label>
+                  <input type="number" id="client-email" placeholder='E-mail do cliente'></input>
+                  
+                  <label for="client-contact-1">Telefone 1</label>
+                  <input type="number" id="client-contact-1" placeholder='Telefone 1'></input>
+
+                  <label for="client-contact-2">Telefone 2</label>
+                  <input type="number" id="client-contact-2" placeholder='Telefone 2'></input>
+                </fieldset>
+                 
+                  
+              </div>
+              
+              <div class="search-clients-buttons">
+                  <button class="btn-close" type="">Fechar</button>
+              </div>
+
+          </form>
+      </div>
+        
+
+        <h1 class="client-list">Lista de Clientes Vertis</h1>
       <table id="clientes-table">
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Nome</th>
-                <th>CNPJ/CPF</th>
-                <th>Tipo Acesso</th>
-                <th>Número Acesso</th>
-                <th>Email</th>
-                <th>Telefone</th>
+                <th>Nome do Cliente</th>
+                <th>TeamViewer</th>
+                <th>Senha TeamViewer</th>
+                <th>AnyDesk</th>
+                <th>Senha AnyDesk</th>
+                <th>Versão Vertis</th>
             </tr>
         </thead>
         <tbody>
