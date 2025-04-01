db771200fa50d442efd589fbade6076599734e61
Kleyton Holanda <69331002+Kehs4@users.noreply.github.com>
2025-04-01 12:56:30 -0300

    change colors and icons, planweb 2.1

--- planilhaweb/src/App.jsx
+++ planilhaweb/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         <header>
           <nav>
               <div class="nav-title">
-                <a href="https://vertisvet.com.br" target='blank'><img src="./vertislogo.png" width={150} height={45}></img></a>
+                <a href="https://vertisvet.com.br" target='blank'><img src="./vertisheader.png" width={105} height={35}></img></a>
                   <h1 class="gpi-title">GPI - Planilha de Clientes</h1>
               </div>
           </nav>
@@ -52,58 +52,58 @@ function App() {
                 <fieldset>
                 <legend>Dados do Cliente</legend>
                   <label for="client-name">Nome do Cliente</label>
-                  <input type="text" id="client-name-input" placeholder="Nome do Cliente" disabled></input>
+                  <input type="text" class='btns' id="client-name-input" placeholder="Nome do Cliente" disabled></input>
                     
                   <label for="client-cod">Código do Cliente</label>
-                  <input type="number" id="client-cod" placeholder="Código do Cliente" disabled></input>
+                  <input type="number" class='btns' id="client-cod" placeholder="Código do Cliente" disabled></input>
 
                   <label for="client-version">Versão do Vertis</label>
-                  <input type="text" id="client-version" placeholder="Versão do Vertis" disabled></input>
+                  <input type="text" class='btns' id="client-version" placeholder="Versão do Vertis" disabled></input>
                 
                   <label for="client-maintenance">Manutenção Realizada</label>
-                  <input type="text" id="client-maintenance" placeholder="Manutenção do Vertis" disabled></input>
+                  <input type="text" class='btns' id="client-maintenance" placeholder="Manutenção do Vertis" disabled></input>
 
                   <label for="client-maintenance">Manutenção Futura</label>
-                  <input type="text" id="client-maintenance-future" placeholder="Próxima Manutenção" disabled></input>
+                  <input type="text" class='btns' id="client-maintenance-future" placeholder="Próxima Manutenção" disabled></input>
                 
                   <label for="client-licences">Licenças da Unidade</label>
-                  <input type="text" id="client-licences" placeholder="Total de Licenças" disabled></input>
+                  <input type="text" class='btns' id="client-licences" placeholder="Total de Licenças" disabled></input>
                 </fieldset>
                     
                 <fieldset>
                   <legend>Dados de Acesso</legend>
                   <label for="client-teamviewer">TeamViewer</label>
-                  <input type="text" id="client-teamviewer" placeholder="TeamViewer do cliente" disabled></input>
+                  <input type="text" class='btns' id="client-teamviewer" placeholder="TeamViewer do cliente" disabled></input>
 
                   <label for="client-teamviewer-password">Senha TeamViewer</label>
-                  <input type="text" id="client-teamviewer-password" placeholder="Senha do TeamViewer" disabled></input>
+                  <input type="text" class='btns' id="client-teamviewer-password" placeholder="Senha do TeamViewer" disabled></input>
 
                   <label for="client-anydesk">AnyDesk</label>
-                  <input type="text" id="client-anydesk" placeholder="AnyDesk do cliente" disabled></input>
+                  <input type="text" class='btns' id="client-anydesk" placeholder="AnyDesk do cliente" disabled></input>
 
                   <label for="client-anydesk-password">Senha AnyDesk</label>
-                  <input type="text" id="client-anydesk-password" placeholder="Senha do AnyDesk" disabled></input>
+                  <input type="text" class='btns' id="client-anydesk-password" placeholder="Senha do AnyDesk" disabled></input>
 
                   <label for="client-licences">IP Servidor do Cliente</label>
-                  <input type="text" id="client-ip-server" placeholder="IP" disabled></input>
+                  <input type="text" class='btns' id="client-ip-server" placeholder="IP" disabled></input>
                 </fieldset>
                   
                 <fieldset>
                   <legend>Contatos</legend>
                   <label for="client-manager">Responsável Unidade</label>
-                  <input type="text" id="client-manager" placeholder='Responsável pela unid.' disabled></input>
+                  <input type="text" class='btns' id="client-manager" placeholder='Responsável pela unid.' disabled></input>
                   
                   <label for="client-email">E-mail</label>
-                  <input type="text" id="client-email" placeholder='E-mail do Cliente' disabled></input>
+                  <input type="text" class='btns' id="client-email" placeholder='E-mail do Cliente' disabled></input>
                   
                   <label for="client-ddd">DDD</label>
-                  <input type="text" id="client-ddd" placeholder='DDD do Cliente' disabled></input>
+                  <input type="text" class='btns' id="client-ddd" placeholder='DDD do Cliente' disabled></input>
 
                   <label for="client-contact-1">Telefone 1</label>
-                  <input type="text" id="client-contact-1" placeholder='Telefone 1' disabled></input>
+                  <input type="text" class='btns' id="client-contact-1" placeholder='Telefone 1' disabled></input>
 
                   <label for="client-contact-2">Telefone 2</label>
-                  <input type="text" id="client-contact-2" placeholder='Telefone 2' disabled></input>
+                  <input type="text" class='btns' id="client-contact-2" placeholder='Telefone 2' disabled></input>
                 </fieldset>
                   
               </div>
@@ -118,6 +118,8 @@ function App() {
         </div>
       </div>
         
+    <img class='vertisimg' src='/public/vertislogo2.png'></img>
+
       <table id="clientes-table">
         <thead>
             <tr>
@@ -141,7 +143,7 @@ function App() {
       <footer class='footer-vertis'>
         <div class='div-all-footer'>
                 <div class='footer-logo'>
-                  <a href="https://vertisvet.com.br" target='_blank'><img src='./public/vertislogo.png' width={230} height={70}></img></a>
+                  <a href="https://vertisvet.com.br" target='_blank'><img src='./public/vertislogo.png' width={150} height={100}></img></a>
                   <p>© 2025 por Vertis.</p>
                 </div>
 
@@ -165,7 +167,7 @@ function App() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                       <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
                     </svg>
-                    <a href="mailto:contato@gpiti.com.br"><p>contato@gpiti.com.br</p></a>
+                    <a href="mailto:contato@gpiti.com.br" class='a-contact'><p>contato@gpiti.com.br</p></a>
                 </div>
 
                 <div class='telefone-svg'>
@@ -179,18 +181,24 @@ function App() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
                       <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
                     </svg>
-                    <a href="https://wa.me/5511964410631" target='_blank'><p>(11) 96441-0631</p></a>
+                    <a href="https://wa.me/5511964410631" class='a-whatsapp' target='_blank'><p>(11) 96441-0631</p></a>
                 </div>
 
                 <div class='instagram-logo'>
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
                     <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
                   </svg>
-                  <a href='https://instagram.com/sistemavertis' target='_blank'><p>@sistemavertis</p></a>
+                  <a href='https://instagram.com/sistemavertis' class='a-instagram' target='_blank'><p>@sistemavertis</p></a>
                 </div>
+
               </div> 
 
         </div>
+        <div class='footer-local'>
+            <p>Av. Engenheiro Armando de Arruda Pereira, 2937 - Bloco A - Sala 115.</p>
+            <p>Jabaquara - São Paulo - SP, CEP: 04309-011.</p>
+        </div>
+        
       </footer>
       </>
   )
